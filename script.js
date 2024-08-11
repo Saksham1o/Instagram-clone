e734eb211e8b4088cb6c2361a1d093d2980d7587
Saksham1o <sakshampatiyar26@gmail.com>
2024-08-11 23:58:55 +0530

    menu and profile button styling

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('Code is Poetry');
+
+const toggleThemeBtn = document.querySelector('.header__theme-button');
+const storiesContent = document.querySelector('.stories__content');
+const storiesLeftButton = document.querySelector('.stories__left-button');
+const storiesRightButton = document.querySelector('.stories__right-button');
+const posts = document.querySelectorAll('.post');
+const postsContent = document.querySelectorAll('.post__content');
+
+document.onload = setInitialTheme(localStorage.getItem('theme'));
+function setInitialTheme(themeKey) {
+  if (themeKey === 'dark') {
+    document.documentElement.classList.add('darkTheme');
+  } else {
+    document.documentElement.classList.remove('darkTheme');
+  }
+}
+
